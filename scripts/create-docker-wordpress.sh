cef2b08de7241c97539c5e66d5bdf4a201aa5d57
Joshua Reynolds <josh@gistlabs.com>
2015-03-21 00:29:05 +0000

    simplifying creation script to create the data volumes if they don't exist or use them if they do.

--- scripts/create-docker-wordpress.sh
+++ scripts/create-docker-wordpress.sh
@@ -2,7 +2,7 @@
 
 scriptname=`basename $0`
 arguments="WP_ROOT_NAME WP_PORT MYSQL_ROOT_PASSWORD VOLUMES_DIRECTORY"
-purpose="This script is for creating a new wordpress set of docker containers that point at existing data volumes"
+purpose="This script is for creating a new wordpress set of docker containers. If volumes exist for this wordpress root in the VOLUMES_DIRECTORY, they will be used, otherwise they will be created." "
 
 argcount=`wc -w <<< $arguments`
 
@@ -25,27 +25,8 @@ mysql_data_volume=$volumes_directory/${siteroot}/mysql
 wp_container=${siteroot}_wp
 mysql_container=${siteroot}_mysql
 
-if [ ! -e $wp_data_volume ]
-then
-	echo
-	echo "ERROR: Wordpress Data Volume does not exist"
-	echo
- 	echo "$wp_data_volume"
-	echo
-	exit 1
-fi
-
-if [ ! -e $mysql_data_volume ]
-then
-	echo
-	echo "ERROR: MySQL Data Volume does not exist."
-	echo
-	echo "$mysql_data_volume"
-	echo
-	echo "Delete and try again"
-	echo
-	exit 1
-fi
+mkdir -p $wp_data_volume
+mkdir -p $mysql_data_volume:
 
 docker run --name $mysql_container \
 	   --volume $mysql_data_volume:/var/lib/mysql \
